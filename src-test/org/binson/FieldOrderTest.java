f09dd888c3c56c14f5b20f5295074a6985eedd0c
frans.lundberg@assaabloy.com <frans.lundberg@assaabloy.com>
2016-01-28 09:07:24 +0100

    tuny update

--- src-test/org/binson/FieldOrderTest.java
+++ src-test/org/binson/FieldOrderTest.java
@@ -8,10 +8,10 @@ import org.junit.Test;
 public class FieldOrderTest {	
 	@Test
 	public void testAlex() {
-		// Reported 2016-01-28 by Alex.
+		// Reported 2016-01-28 by Alexander Reshniuk
 		
-		byte[] b1 = {(byte)0xf3, (byte)0x91, (byte)0xb0, (byte)0xa7};
-		byte[] b2 = {(byte)0xef, (byte)0xad, (byte)0xab, (byte)0xe8, (byte)0xb0, (byte)0xa6};
+		byte[] b1 = {(byte)0xf3, (byte)0x91, (byte)0xb0, (byte)0xa7};  // UTF16: DB07 DC27
+		byte[] b2 = {(byte)0xef, (byte)0xad, (byte)0xab, (byte)0xe8, (byte)0xb0, (byte)0xa6}; // UTF16: FB6B 8C26 
 		String s1 = new String(b1, StandardCharsets.UTF_8);		
 		String s2 = new String(b2, StandardCharsets.UTF_8);
 		
