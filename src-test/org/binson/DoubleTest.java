8e143297d7b204c4ed94906cd509bd23ff3d8db6
frans.lundberg@assaabloy.com <frans.lundberg@assaabloy.com>
2017-03-24 13:26:58 +0100

    more tests

--- src-test/org/binson/DoubleTest.java
+++ src-test/org/binson/DoubleTest.java
@@ -2,6 +2,8 @@ package org.binson;
 
 import static org.junit.Assert.*;
 
+import org.binson.lowlevel.Hex;
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -28,5 +30,36 @@ public class DoubleTest {
     @Test(expected=FormatException.class)
     public void testNonExistant() {
         obj.getDouble("b");
+    }
+    
+    @Test
+    public void testPi() {
+        byte[] bytes = new Binson().put("pi", Math.PI).toBytes();
+        //System.out.println(Hex.create(bytes));
+        // 401402706946182d4454fb21094041
+        
+        Binson obj = Binson.fromBytes(bytes);
+        Assert.assertTrue(Math.PI == obj.getDouble("pi"));
+    }
+    
+    @Test
+    public void testNaN() {
+        byte[] bytes = new Binson().put("nan", Double.NaN).toBytes();
+        //System.out.println(Hex.create(bytes));
+        // 4014036e616e46000000000000f87f41
+        
+        Binson obj = Binson.fromBytes(bytes);
+        Assert.assertTrue(Double.isNaN(obj.getDouble("nan")));
+    }
+    
+    @Test
+    public void testInf() {
+        byte[] bytes = new Binson().put("inf", Double.POSITIVE_INFINITY).toBytes();
+        
+        //System.out.println(Hex.create(bytes));
+        // 401403696e6646000000000000f07f41
+        
+        Binson obj = Binson.fromBytes(bytes);
+        Assert.assertTrue(Double.POSITIVE_INFINITY == obj.getDouble("inf"));
     }
 }
